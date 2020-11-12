026262de1d8286513e2f041794b9e19351643489
Raziescudero <51581559+Raziescudero@users.noreply.github.com>
2020-11-11 18:34:29 -0600

    Add files via upload

--- src/Controlador/EstudianteProductoController.java
+++ src/Controlador/EstudianteProductoController.java
@@ -9,11 +9,20 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import modelo.ProductoVO;
+import modelo.Producto_DAO_IMP;
 import modelo.Sugerir_Pro;
 import modelo.Sugerir_Pro_DAO_IMP;
 
@@ -30,18 +39,60 @@ public class EstudianteProductoController implements Initializable {
     private JFXTextArea textDesc;
     @FXML
     private JFXButton botonAgregar;
+    @FXML
+    private TableView<ProductoVO> tvProducto;
+    @FXML
+    private TableColumn<ProductoVO, String> tabNombre;
+    @FXML
+    private TableColumn<ProductoVO, String> tabDesc;
+    @FXML
+    private TableColumn<ProductoVO, Integer> tabCanti;
+    @FXML
+    private TableColumn<ProductoVO, Integer> tabPrecio;
+    
     Sugerir_Pro_DAO_IMP implementacionSugerirDAO=new Sugerir_Pro_DAO_IMP();
     String nombreAux="";
+    private ProductoVO producto = new ProductoVO();
+    private Producto_DAO_IMP imp = new Producto_DAO_IMP();
+    private ObservableList<ProductoVO> listaDeProductos;
+
     
 
     /**
      * Initializes the controller class.
      * @param url
      */
+    
+        public void obtenerProducto(){
+        List listaConsulta = null;
+        try{
+            listaConsulta = imp.readAll();
+        } catch(Exception e){
+            System.out.println("Error al leer la consulta");
+        }
+        Iterator it = listaConsulta.iterator();
+       listaDeProductos.clear();
+       while(it.hasNext()){
+           listaDeProductos.add((ProductoVO)it.next());
+       }
+    
+    }
+    
+    public void colocarProductosTabla(){
+        this.obtenerProducto();
+        this.tabNombre.setCellValueFactory(new PropertyValueFactory<>("Nombre"));
+        this.tabDesc.setCellValueFactory(new PropertyValueFactory<>("Descripcion"));
+        this.tabCanti.setCellValueFactory(new PropertyValueFactory<>("Cantidad"));
+        this.tabPrecio.setCellValueFactory(new PropertyValueFactory<>("Precio"));
+        this.tvProducto.setItems(listaDeProductos);
+    }
+    
     @Override
 
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       this.imp = new Producto_DAO_IMP();
+       this.listaDeProductos = FXCollections.observableArrayList();
+       this.colocarProductosTabla();
     }    
 
     
