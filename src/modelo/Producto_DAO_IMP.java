8ac96f698e1259bc9d96ca0356256d356f9bfd08
Raziescudero <51581559+Raziescudero@users.noreply.github.com>
2020-11-26 22:22:59 -0600

    Add files via upload

--- src/modelo/Producto_DAO_IMP.java
+++ src/modelo/Producto_DAO_IMP.java
@@ -7,9 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Producto_DAO_IMP implements Producto_DAO{
+public class Producto_DAO_IMP {
     
-    @Override
     public boolean create(ProductoVO producto) throws Exception{
         boolean created = false;
         Statement st;
@@ -35,7 +34,6 @@ public class Producto_DAO_IMP implements Producto_DAO{
         return created;
     }
     
-    @Override
     public List<ProductoVO> readAll() throws Exception{
         Connection con = null;
         Statement stm = null;
@@ -65,106 +63,25 @@ public class Producto_DAO_IMP implements Producto_DAO{
     
     }
     
-    
-    @Override
-    public ProductoVO read(String nombre) throws Exception {
+    public boolean update(ProductoVO producto) throws Exception{
         Connection con = null;
         Statement stm = null;
-        ResultSet rs = null;
-        String sql = "SELECT * FROM producto WHERE Nombre ='" + nombre + "'";
-
-        ProductoVO producto = new ProductoVO();
-
-        try {
-            con = new ConexionDB().conectarMySQL();
-            stm = con.createStatement();
-            rs = stm.executeQuery(sql);
-            
-            while (rs.next()) {
-                producto.setId(rs.getInt(1));
-                producto.setNombre(rs.getString(2));
-                producto.setDescripcion(rs.getString(3));
-                producto.setCantidad(rs.getInt(4));
-                producto.setPrecio(rs.getInt(5));
-            }
-            stm.close();
-            rs.close();
-            con.close();
-        } catch (Exception e) {
-           e.printStackTrace();
-        }
-        return producto;      
-    }
-
-    @Override
-    public boolean reservar(int  idProducto, String matricula) throws Exception{
-       
-        boolean reservado = false;
-        Statement st;
-        Connection conn;
-        String sql = "INSERT INTO pedido values(NULL, '"+ matricula +"',"+"'"+ idProducto +"')";
-        ConexionDB co = new ConexionDB();
-        try{
-            conn = co.conectarMySQL();
-            st = conn.createStatement();
-            st.execute(sql);
-            reservado = true;
-            st.close();
-            conn.close();
-        } catch(SQLException e){
-            throw new Exception ("Error SQLException "+e.getMessage());
-        } catch(NullPointerException e){
-            throw new Exception("Error "+e.getMessage());
-        } catch(Exception e){
-            throw new Exception("Error "+e.getMessage());
-        }
         
-        return reservado;
-    }
-
-    @Override
-    public ProductoVO readId(int id) throws Exception {
-        Connection con = null;
-        Statement stm = null;
-        ResultSet rs = null;
-        String sql = "SELECT * FROM producto WHERE id ='" + id + "'";
-
-        ProductoVO producto = new ProductoVO();
-
-        try {
+        boolean actualizar = false;
+        
+        String sql = "UPDATE producto set Nombre='" + producto.getNombre() + "', Descripcion='" + producto.getDescripcion()
+                + "', Cantidad='" + producto.getCantidad() + "', Precio='" + producto.getPrecio() + "' WHERE ID ='"
+                + producto.getId() + "'";
+        try{
             con = new ConexionDB().conectarMySQL();
             stm = con.createStatement();
-            rs = stm.executeQuery(sql);
-            
-            while (rs.next()) {
-                producto.setId(rs.getInt(1));
-                producto.setNombre(rs.getString(2));
-                producto.setDescripcion(rs.getString(3));
-                producto.setCantidad(rs.getInt(4));
-                producto.setPrecio(rs.getInt(5));
-            }
-            stm.close();
-            rs.close();
-            con.close();
-        } catch (Exception e) {
-           e.printStackTrace();
+            stm.execute(sql);
+            actualizar = true;
+        } catch (SQLException e){
+            throw new Exception ("Error en SQLException " + e.getCause().toString());
+        } catch(Exception e){
+            throw new Exception ("Error en Exception " + e.getMessage().toString());
         }
-        return producto;  
+        return actualizar;
     }
-
-    @Override
-    public ProductoVO search(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean update(ProductoVO producto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean delete(ProductoVO producto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
