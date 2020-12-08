8d1d88316750f4a3766da810c321b19c01fe13a3
enriqueconss <56031747+enriqueconss@users.noreply.github.com>
2020-12-08 10:41:52 -0600

    S6
    Iteración 6 - Unificar ventana

--- src/proyectocafeteria/ProyectoCafeteria.java
+++ src/proyectocafeteria/ProyectoCafeteria.java
@@ -30,8 +30,8 @@ public class ProyectoCafeteria extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_EstudiantesFXML.fxml"));
        //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
        //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/LoginCafeteriaFXML.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/LoginCafeteriaFXML.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Login");
         stage.setScene(scene);
