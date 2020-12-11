520d65e8e912cda5149915218f15fb75a6d3a621
junge49 <joelhesh309@gmail.com>
2020-12-11 02:22:17 -0600

    Reparaciones y FiltarMenú

--- src/proyectocafeteria/ProyectoCafeteria.java
+++ src/proyectocafeteria/ProyectoCafeteria.java
@@ -29,13 +29,13 @@ public class ProyectoCafeteria extends Application {
 
         //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/LoginCafeteriaFXML.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_EstudiantesFXML.fxml"));
-       //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
        //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/LoginCafeteriaFXML.fxml"));
         
        //Parent root = FXMLLoader.load(getClass().getResource("/VentanaAux/AdministradorHorario.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/Ventanas/Ventana_AdministradorFXML.fxml"));
         
 
         Scene scene = new Scene(root);
